f0419e2a2b6adfd36aadd0f895dbd9d1766a656b
treasurytrader <rbooja@outlook.com>
2025-07-03 03:51:00 +0900

    Create real-time_mouse_cursor_position.ps1

---
+++ PowerShell/real-time_mouse_cursor_position.ps1
@@ -0,0 +1,25 @@
+﻿
+#+------------------------------------------------------------------+
+#|                                                                  |
+#+------------------------------------------------------------------+
+
+Add-Type -AssemblyName System.Windows.Forms
+
+while ($true) {
+
+    $mousePosition = [System.Windows.Forms.Cursor]::Position
+
+    $xCoord = $mousePosition.X
+    $yCoord = $mousePosition.Y
+
+    Clear-Host
+    Write-Host "Real-time mouse cursor position x coordinate: $xCoord"
+    Write-Host "Real-time mouse cursor position y coordinate: $yCoord"
+
+    Start-Sleep -Milliseconds 100  # Adjust the delay as needed
+
+}
+
+#+------------------------------------------------------------------+
+#|                                                                  |
+#+------------------------------------------------------------------+
