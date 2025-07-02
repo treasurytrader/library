66b9999b47c7d591308e3e291610682e4aea488a
treasurytrader <rbooja@outlook.com>
2025-07-03 03:50:54 +0900

    Create download.ps1

---
+++ PowerShell/download.ps1
@@ -0,0 +1,9 @@
+﻿
+# 받운로드 받고 싶은 주소
+$source = 'http://abc.def.com/01.mp4'
+
+# 다운로드 받아서 어디에 저장 할 것인지
+$destination = 'D:\Download\01.mp4'
+
+# 다운로드 시작
+Invoke-WebRequest -Uri $source -OutFile $destination
