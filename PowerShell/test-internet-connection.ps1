6fd5233c882e3307064c0e277d604b2a1f023863
treasurytrader <rbooja@outlook.com>
2025-07-03 03:51:04 +0900

    Create test-internet-connection.ps1

---
+++ PowerShell/test-internet-connection.ps1
@@ -0,0 +1,25 @@
+﻿
+#+------------------------------------------------------------------+
+#|                                                                  |
+#+------------------------------------------------------------------+
+
+function TestInternet {
+
+  $Result = Test-Connection -ComputerName 8.8.8.8 -ErrorAction SilentlyContinue
+
+  if ($? -eq $true) {
+    write-output "Internet: OK"
+    return $true
+  }
+  else {
+    write-output "Internet: Error"
+    return $false
+  }
+
+}
+
+TestInternet
+
+#+------------------------------------------------------------------+
+#|                                                                  |
+#+------------------------------------------------------------------+
