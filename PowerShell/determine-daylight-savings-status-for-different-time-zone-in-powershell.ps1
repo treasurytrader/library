0b6db216eb79ae57ec7be2b9f2442fa5a4123f43
treasurytrader <rbooja@outlook.com>
2025-07-03 03:50:52 +0900

    Create determine-daylight-savings-status-for-different-time-zone-in-powershell.ps1

---
+++ PowerShell/determine-daylight-savings-status-for-different-time-zone-in-powershell.ps1
@@ -0,0 +1,18 @@
+﻿
+#+------------------------------------------------------------------+
+#|                                                                  |
+#+------------------------------------------------------------------+
+# https://stackoverflow.com/questions/44781312/determine-daylight-savings-status-for-different-time-zone-in-powershell
+
+# collect your input (through db, or whatever mechanism you have)
+$inputDateTime = Get-Date -Date "2017-06-27 00:00:00"
+
+# pick your time zones
+$fromTimeZone = "Central Europe Standard Time"  # EU
+$toTimeZone   = "Central Standard Time"         # USA
+
+# convert
+$outputDateTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($inputDateTime, $fromTimeZone, $toTimeZone)
+
+# output
+Write-Output $outputDateTime
